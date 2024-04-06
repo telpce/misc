2f3d7ff39e212da1ece3458f5bb29bfd506667b4
telpce <telpce@ymail.ne.jp>
2024-04-06 14:33:39 +0900

    ログイン中の人が居るときにsteamの処理をしないように改善

--- src/palworld_auto_update.ps1
+++ src/palworld_auto_update.ps1
@@ -1,4 +1,4 @@
-# load windows environment variable
+# Load windows environment variable
 $adminPassword = $Env:PALADMINPASSWORD
 $port = $Env:PALPORT
 
@@ -15,21 +15,21 @@ $shutdown_body = @"
 }
 "@
 
-# Update settings
-$jsonData = Invoke-WebRequest https://api.steamcmd.net/v1/info/2394010
-$parsedData = $jsonData  | ConvertFrom-Json
-$data = $parsedData.data.2394010
-# $public_build_id = $data.depots.branches.public.buildid
-$win = $data.depots.2394011
-$public_win_gid = $win.manifests.public.gid
-
-$steamcmd = C:\dedicated_pl\steamcmd +login anonymous +app_status 2394010 +quit
-$line = $steamcmd | Select-String '2394011 :'
-$local_win_gid = [regex]::Match($line, 'manifest (\d+)').Groups[1].Value
-#$local_build_id = [regex]::Match($steamcmd, 'BuildID (\d+)').Groups[1].Value
-
 $players = Invoke-RestMethod "http://localhost:8212/v1/api/players" -Method 'GET' -Headers $headers
 if ($players.players.Count -eq 0) {
+
+  # Update settings
+  $jsonData = Invoke-WebRequest https://api.steamcmd.net/v1/info/2394010
+  $parsedData = $jsonData  | ConvertFrom-Json
+  $data = $parsedData.data.2394010
+  # $public_build_id = $data.depots.branches.public.buildid
+  $win = $data.depots.2394011
+  $public_win_gid = $win.manifests.public.gid
+
+  $steamcmd = C:\dedicated_pl\steamcmd +login anonymous +app_status 2394010 +quit
+  $line = $steamcmd | Select-String '2394011 :'
+  $local_win_gid = [regex]::Match($line, 'manifest (\d+)').Groups[1].Value
+  #$local_build_id = [regex]::Match($steamcmd, 'BuildID (\d+)').Groups[1].Value
 
   if ($local_win_gid -ne $public_win_gid) {
 
