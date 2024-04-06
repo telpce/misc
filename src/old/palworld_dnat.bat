5c26cf7930baa23860fd9f509461a68924f5c07a
telpce <telpce@ymail.ne.jp>
2024-04-06 12:41:04 +0900

    winDNATに補足追加

--- src/old/palworld_dnat.bat
+++ src/old/palworld_dnat.bat
@@ -1,1 +1,2 @@
+: note: you need to ADD ip address(130.211.23.183/32) to windows
 netsh interface portproxy add v4tov4 listenaddress=130.211.23.183 listenport=443 connectaddress=*.*.*.* connectport=9999
